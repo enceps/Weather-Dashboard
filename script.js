321b4e4c98c9f30923cb0db0d890a57063bb8012
enceps <spence.d.cole@gmail.com>
2022-08-10 14:49:44 -0400

    pull temp from api data

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
 
 var city = "london";
 var apiKey =  "52f6964e61a82e1b57d7417070c8b005";
-let local = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" + apiKey +"";
- fetch(local).then(function(response){
+ let weather = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=52f6964e61a82e1b57d7417070c8b005";
+ fetch(weather).then(function(response){
     if (response.ok){
         response.json().then(function(data){
             console.log(data);
+         console.log(response);
+         console.log(data.main.temp);
         });
+   
     }
         else {
-            alert("There was a problem!")
+            alert("problem")
         }
-    }
- );
-
-
-
-
-var data = "data";
-console.log(data);
+ });
